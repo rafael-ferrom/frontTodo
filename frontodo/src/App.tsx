d32c8bb609a1acf9aaffe3573346f77336e4c445
rafael-ferrom <rafaelferromac@gmail.com>
2026-04-12 21:45:35 -0300

    feat: create route for application

--- frontodo/src/App.tsx
+++ frontodo/src/App.tsx
@@ -1,12 +1,41 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageLayout from "./Layout/PageLayout";
+import StartPage from "./pages/StartPage/StartPage";
+import TodoPage from "./pages/todoPage/TodoPage";
+import UserPage from "./pages/userPage/UserPage";
+import MainPage from "./pages/mainPage/MainPage";
 
 function App() {
 
+   const router = createBrowserRouter(
+    [
+  {
+    path: "/",
+    element: <PageLayout></PageLayout>,
+    children: [
+      {
+        path: "/",
+        element: <StartPage></StartPage>,
+      },
+      {
+        path: "/Fetch",
+        element: <TodoPage></TodoPage> ,
+      },
+      {
+        path: "/Axios",
+        element: <UserPage></UserPage>,
+      },
+      {
+        path: "/Thunk",
+        element: <MainPage></MainPage>,
+      },
+    ],
+  },
+]
+   )
 
   return (
-    <>
-      
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
